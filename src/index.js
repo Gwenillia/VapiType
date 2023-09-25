755437d98dc942838d64178ec4d73854910add2a
Gwenillia <gwenden31@gmail.com>
2023-09-25 23:31:56 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import englishTranslation from './locales/en/en.json';
+import frenchTranslation from './locales/fr/fr.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: englishTranslation
+    },
+    fr: {
+      translation: frenchTranslation
+    }
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
